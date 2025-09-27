cafa7f05c9d7fa422aa8cf2824613ba701cf89bf
Aryan <aryan7017n@gmail.com>
2025-09-27 13:31:55 +0530

    cources-hemburger added

--- home.js
+++ home.js
@@ -1,7 +1,11 @@
 let hemburgerMenu = document.querySelector("#hemburger-menu");
 let hemburgerBox = document.querySelector("#hemburger-nav-links");
 let hemburgerCloseBtn = document.querySelector("#hemburger-close-btn");
+let courcesNev = document.querySelector("#cources-navLink");
+let courcesHempbuger = document.querySelector("#cources-hemburger");
 
+
+console.log(courcesNev, courcesHempbuger);
 
 hemburgerMenu.addEventListener("click", () => {
     hemburgerBox.classList.toggle("hidden");
@@ -9,4 +13,22 @@ hemburgerMenu.addEventListener("click", () => {
 
 hemburgerCloseBtn.addEventListener("click", () => {
     hemburgerBox.classList.toggle("hidden");
-})
+});
+
+courcesNev.addEventListener("mouseenter", () => {
+    courcesHempbuger.classList.remove("hidden");
+});
+
+
+
+courcesHempbuger.addEventListener("mouseleave", () => {
+    courcesHempbuger.classList.add("hidden");
+});
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY >= 200) {
+        courcesHempbuger.classList.add("hidden");
+        console.log("Working")
+    }
+    console.log(window.scrollY);
+});

8d2c9f5930fa4649d56582dc90ba0444fb630633
Aryan <aryan7017n@gmail.com>
2025-09-30 15:12:13 +0530

    tab fixed

--- courses.js
+++ courses.js
@@ -34,21 +34,4 @@ window.addEventListener("scroll", () => {
 });
 
 
-// tabs
 
-const tabs = document.querySelectorAll(".tab_btn");
-const all_content = document.querySelectorAll(".content");
-
-
-tabs.forEach((tab, index) => {
-    tab.addEventListener("click", (e) => {
-        tabs.forEach(tab => {tab.classList.remove("active")});
-        tab.classList.add("active");
-        const line = document.querySelector(".line");
-        line.style.width = e.target.offsetWidth + "px";
-        line.style.left = e.target.offsetLeft + "px";
-
-        all_content.forEach(content => {content.classList.add("hidden")});
-        all_content[index].classList.remove("hidden");
-    })
-})
